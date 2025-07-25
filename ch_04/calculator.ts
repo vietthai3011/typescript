ae860d3a8bc8ef64373f7ee4561206a5a6e5fea7
vietthai3011 <ngovietthai3011@email.com>
2025-07-25 16:15:23 +0700

    chapter 4

---
+++ ch_04/calculator.ts
@@ -0,0 +1,71 @@
+type InvestmentData = {
+    initialAmount: number,
+    annualContribution: number,
+    expectedRetune: number,
+    duration: number,
+}
+
+type InvestmentResult = {
+    year: string,
+    totalAmount: number,
+    totalContribution: number,
+    totalInterestEarned: number
+}
+
+type CalculationResult = InvestmentResult[] | string;
+
+function calculatorInvestment(data: InvestmentData): CalculationResult {
+    const {initialAmount, annualContribution, expectedRetune, duration} = data;
+
+    if (initialAmount < 0) return "Initial investment amount must be at least zero.";
+    if (duration <= 0) return "No valid amound of year provided.";
+    if (expectedRetune < 0) return "Expected return must be at least zero.";
+
+    let total = initialAmount;
+    let totalContribution = 0;
+    let totalInterestEarned = 0;
+
+    let annualResult: InvestmentResult[] = [];
+
+    for (let i = 0; i < duration; i++) {
+        total = total * (1 + expectedRetune);
+        totalInterestEarned = total - totalContribution - initialAmount;
+        totalContribution = totalContribution + annualContribution;
+        total = total + annualContribution;
+
+        annualResult.push({
+            year: `Year ${i + 1}`,
+            totalAmount: total,
+            totalContribution: totalContribution,
+            totalInterestEarned,
+        });
+    }
+
+    return annualResult;
+}
+
+function printResults(results: CalculationResult) {
+    if (typeof results === "string") {
+        console.log(results);
+        return;
+    }
+
+    results.forEach(yearnAndResult => {
+        console.log(yearnAndResult.year);
+        console.log(`Total: ${yearnAndResult.totalAmount.toFixed(0)}`);
+        console.log(`Total Contribution: ${yearnAndResult.totalContribution.toFixed(0)}`);
+        console.log(`Total Interest Earned: ${yearnAndResult.totalInterestEarned.toFixed(0)}`);
+        console.log("------------------------------");
+    });
+}
+
+const investmentData: InvestmentData = {
+    initialAmount: 5000,
+    annualContribution: 500,
+    expectedRetune: 0.08,
+    duration: 10,
+};
+
+const results = calculatorInvestment(investmentData);
+
+printResults(results);
